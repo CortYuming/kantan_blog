dd3a8fb60b633cd577bd8aff61eec066358651e3
Takashi ITO <cortyuming@gmail.com>
2012-07-27 10:51:30 +0900

    fix settings.py: when local db

--- apps/settings.py
+++ apps/settings.py
@@ -9,23 +9,26 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-## use postgres in heroku
-DEBUG = False
-import dj_database_url
-DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
+import socket
+if "local" in socket.gethostname():
+    ## use sqlite3 in local
+    DEBUG = True
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+            'NAME': 'db.sqlite3',                      # Or path to database file if using sqlite3.
+            'USER': '',                      # Not used with sqlite3.
+            'PASSWORD': '',                  # Not used with sqlite3.
+            'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+            'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+            }
+        }
+else:
+    ## use postgres in heroku
+    DEBUG = False
+    import dj_database_url
+    DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
 
-## use sqlite3 in local
-# DEBUG = True
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-#         'NAME': 'db.sqlite3',                      # Or path to database file if using sqlite3.
-#         'USER': '',                      # Not used with sqlite3.
-#         'PASSWORD': '',                  # Not used with sqlite3.
-#         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-#         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-#         }
-#     }
 
 TEMPLATE_DEBUG = DEBUG
 
